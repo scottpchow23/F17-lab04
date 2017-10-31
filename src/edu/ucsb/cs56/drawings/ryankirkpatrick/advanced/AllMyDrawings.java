b3f2008405f3bd16ef406a37dcf779b795705178
Ryan C. Kirkpatrick <ryankirkpatrick@umail.ucsb.edu>
2017-10-31 16:49:53 -0700

    Changed the package name

--- src/edu/ucsb/cs56/drawings/ryankirkpatrick/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/drawings/ryankirkpatrick/advanced/AllMyDrawings.java
@@ -1,4 +1,4 @@
-package edu.ucsb.cs56.drawings.pconrad.advanced;
+package edu.ucsb.cs56.drawings.ryankirkpatrick.advanced;
 
 import java.awt.Graphics2D;
 import java.awt.Shape; // general class for shapes
