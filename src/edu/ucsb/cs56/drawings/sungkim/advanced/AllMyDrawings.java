f8611754c5323ca761e8e4daa01c764cd485039a
Sung Kim <sungkim@csil-24.cs.ucsb.edu>
2017-10-31 10:39:40 -0700

    fixed more comments

--- src/edu/ucsb/cs56/drawings/sungkim/advanced/AllMyDrawings.java
+++ src/edu/ucsb/cs56/drawings/sungkim/advanced/AllMyDrawings.java
@@ -18,7 +18,7 @@ import edu.ucsb.cs56.drawings.utilities.GeneralPathWrapper;
 
 public class AllMyDrawings
 {
-    /** Draw a picture with a few houses
+    /** Draw a picture with a few flags
      */
 
     public static void drawPicture1(Graphics2D g2) {
@@ -26,14 +26,14 @@ public class AllMyDrawings
         Flag f1 = new Flag(100, 200, 100);
         g2.setColor(Color.CYAN); g2.draw(f1);
 
-	// Make a black house that's half the size,
+	// Make a black flag that's half the size,
 	// and moved over 150 pixels in x direction
 
 	Shape f2 = ShapeTransforms.scaledCopyOfLL(f1,0.5,0.5);
 	f2 = ShapeTransforms.translatedCopyOf(f2,150,0);
 	g2.setColor(Color.BLACK); g2.draw(f2);
 
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a flag that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
 	f2 = ShapeTransforms.scaledCopyOfLL(f2,4,4);
 	f2 = ShapeTransforms.translatedCopyOf(f2,150,0);
@@ -50,7 +50,7 @@ public class AllMyDrawings
 	g2.setColor(new Color(0x002FA7));
 	g2.draw(f2);
 
-	// Draw two houses with Windows
+	// Draw a flag of Greenland
 
 	FlagOfGreenland fg1 = new FlagOfGreenland(50, 100, 150);
 
@@ -65,11 +65,11 @@ public class AllMyDrawings
     }
 
 
-    /** Draw a picture with a few houses and coffee cups
+    /** Draw a picture with a few flags and flags of Greendland
      */
     public static void drawPicture2(Graphics2D g2) {
 
-	// Draw some coffee cups.
+	// Draw some flags and flags of Greenland
 
 	Flag f1 = new Flag(100,50,150);
 	FlagOfGreenland fg1 = new FlagOfGreenland(20,50,30);
@@ -84,13 +84,13 @@ public class AllMyDrawings
 	FlagOfGreenland F1 = new FlagOfGreenland(100,250,175);
 	g2.setColor(Color.CYAN); g2.draw(F1);
 
-	// Make a black house that's half the size,
+	// Make a black flag that's half the size,
 	// and moved over 150 pixels in x direction
 	Shape F2 = ShapeTransforms.scaledCopyOfLL(F1,0.5,0.5);
 	F2 = ShapeTransforms.translatedCopyOf(F2,150,0);
 	g2.setColor(Color.BLACK); g2.draw(F2);
 
-	// Here's a house that's 4x as big (2x the original)
+	// Here's a flag that's 4x as big (2x the original)
 	// and moved over 150 more pixels to right.
 	F2 = ShapeTransforms.scaledCopyOfLL(F2,4,4);
 	F2 = ShapeTransforms.translatedCopyOf(F2,150,0);
@@ -107,7 +107,7 @@ public class AllMyDrawings
 	g2.setColor(new Color(0x002FA7));
 	g2.draw(F2);
 
-	// Draw two houses with Windows
+	// Draw two fkags of Greenland
 
 	FlagOfGreenland fG1 = new FlagOfGreenland(50,350,175);
 	FlagOfGreenland fG2 = new FlagOfGreenland(200,350,200);
@@ -115,7 +115,7 @@ public class AllMyDrawings
 	g2.draw(fG1);
 	g2.setColor(new Color(0x8F00FF));
 
-	// Rotate the second house 45 degrees around its center.
+	// Rotate the second flag 45 degrees around its center.
 	Shape fG3 = ShapeTransforms.rotatedCopyOf(fG2, Math.PI/4.0);
 
 	g2.draw(fG3);
@@ -127,7 +127,7 @@ public class AllMyDrawings
 	g2.drawString("A bunch of flags by Sung Kim", 20,20);
     }
 
-    /** Draw a different picture with a few houses and coffee cups
+    /** Draw a different picture with a few flags of Greenland
      */
 
     public static void drawPicture3(Graphics2D g2) {
@@ -137,7 +137,7 @@ public class AllMyDrawings
 	g2.drawString("A bunch of flags by Sung Kim", 20,20);
 
 
-	// Draw some coffee cups.
+	// Draw some flags of Greenland.
 
 	FlagOfGreenland largeF = new FlagOfGreenland(50,50,50);
 	FlagOfGreenland smallF = new FlagOfGreenland(100,100,100);
